27330120b47e66260992e0ba2ea9670ae33707c5
Katrin Shechtman <katrin.shechtman@gmail.com>
2013-04-12 02:13:02 -0400

    renaming _id to id;aggregated commentsNum in blogentry

--- app/models/BlogEntry.scala
+++ app/models/BlogEntry.scala
@@ -17,9 +17,10 @@ case class BlogEntry(
 		entryType: String,
 		categoryId: String,
 		url: String,
-		tags: Seq[String])
+		tags: Seq[String],
+		commentsNum: Int)
 
-object BlogEntry extends Function9[String, String, String, DateTime, String, String, String, String, Seq[String], BlogEntry] {
+object BlogEntry extends Function10[String, String, String, DateTime, String, String, String, String, Seq[String], Int, BlogEntry] {
 	implicit val recipeWrites = Json.writes[BlogEntry]
 	implicit val recipeReads = Json.reads[BlogEntry]
 }
