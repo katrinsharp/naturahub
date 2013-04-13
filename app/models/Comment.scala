1862b4875adf797998d21fa7c5a59c8bd81ef2b4
Katrin Shechtman <katrin.shechtman@gmail.com>
2013-04-12 22:29:57 -0400

    more _id; user in session if missing handling in recipe view;time in comments to display the greates portion, disable threaded comments for time being

--- app/models/Comment.scala
+++ app/models/Comment.scala
@@ -7,13 +7,12 @@ import org.joda.time.DateTime
 
 case class Comment(
 		id: String,
-		postId: String,
 		content: String,
 		created: DateTime, 
 		by: String,
 		slug: String)
 
-object Comment extends Function6[String, String, String, DateTime, String, String, Comment] {
+object Comment extends Function5[String, String, DateTime, String, String, Comment] {
 	implicit val writes = Json.writes[Comment]
 	implicit val reads = Json.reads[Comment]
 }
